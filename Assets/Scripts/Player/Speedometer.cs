c69381ba0ae0654a991342e3a99654df48585e93
Noé Auguste - STUDENT <s00295651@atu.ie>
2026-04-16 13:01:14 +0100

    added speedlines

--- Assets/Scripts/Player/Speedometer.cs
+++ Assets/Scripts/Player/Speedometer.cs
@@ -1,19 +1,26 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Speedometer : MonoBehaviour
 {
     public PlayerMovement playerMovement;
-
+    public VisualEffect speedEffect;
     public TMP_Text speedText;
 
     public float displayMultiplier = 100f;
 
     void Update()
     {
-        if (playerMovement == null || speedText == null) return;
+        if (playerMovement == null || speedText == null)
+        {
+            return;
+        }
 
         float speed = playerMovement.HorizontalSpeed * displayMultiplier;
+        float ajustedRadius = 1 / Mathf.Log(speed / 250, 12);
+        float clampedRadius = Mathf.Clamp(ajustedRadius, 0.6f, 3f);
+        speedEffect.SetFloat("Radius", clampedRadius);
         speedText.text = Mathf.RoundToInt(speed).ToString();
     }
 }
